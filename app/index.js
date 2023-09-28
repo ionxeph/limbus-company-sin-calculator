53b2b36718f8e5d5e82dbac9aa0d78925dbaa064
andrew-cheng <andrew.cheng@gmail.com>
2023-09-27 22:46:46 -0500

    playing around with styling

--- app/index.js
+++ app/index.js
@@ -11,7 +11,8 @@ function generateInTeamCheckbox(inTeam, sinnerName) {
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.checked = inTeam;
-  checkbox.setAttribute('aria-label', `select ${sinnerName}`)
+  checkbox.setAttribute('aria-label', `select ${sinnerName}`);
+  checkbox.className = 'appearance-none peer w-8 h-8 bg-gray-300 border-gray-300 rounded focus:ring-red-500 focus:ring-2 checked:border-0dark:bg-gray-700 dark:border-gray-600 checked:bg-slate-300 dark:checked:bg-slate-500 dark:focus:ring-red-600 dark:ring-offset-gray-800';
   checkbox.addEventListener('change', () => {
     team.toggle_sinner_selection(sinnerName);
     updateSins();
@@ -28,7 +29,8 @@ teamData.sinners.forEach((sinner, i) => {
   const isInTeamCheckboxContainer = sinnerContainers[i].querySelector('span.in-team-checkbox-container');
   nameContainer.innerHTML = sinner.name;
   selectedIdContainer.innerHTML = sinner.selected_identity.name;
-  isInTeamCheckboxContainer.appendChild(generateInTeamCheckbox(sinner.in_team, sinner.name));
+  isInTeamCheckboxContainer.innerHTML = `<svg class="absolute w-8 h-8 right-0 top-1 fill-slate-900 hidden peer-checked:block pointer-events-none"><use xlink:href="#icon-id" /></svg>`;
+  isInTeamCheckboxContainer.prepend(generateInTeamCheckbox(sinner.in_team, sinner.name));
 });
 
 function updateSins() {
