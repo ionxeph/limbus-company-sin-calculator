1b9e38e77f4eae6b0040f4dd7ce9a2f2648808dd
andrew-cheng <andrew.cheng@gmail.com>
2023-09-28 14:20:38 -0500

    add simple UI for changing ID

--- app/index.js
+++ app/index.js
@@ -1,13 +1,27 @@
 import './styles.css';
 import { Team } from 'limbus-company-team-builder';
-import * as starterData from './starter-data.json';
+import * as rawData from './data.json';
 
 // TODO: work on light mode styling
-let team = Team.load(JSON.stringify(starterData));
+const initialData = JSON.parse(JSON.stringify(rawData));
+let team = Team.load(JSON.stringify(rawData));
 
 // team.change_selected_id("Yi Sang", "Seven Association South Section 6");
 
-let initialData = JSON.parse(team.as_json_string());
+function generateIdSelector(sinnerName, identities, selected) {
+  const selector = document.createElement('select');
+  identities.forEach(id => {
+    const option = document.createElement('option');
+    option.innerHTML = id.name;
+    option.selected = id.name === selected.name;
+    selector.appendChild(option);
+  });
+  selector.addEventListener('change', () => {
+    team.change_selected_id(sinnerName, selector.value);
+    updateSins();
+  });
+  return selector;
+}
 
 function generateInTeamCheckbox(inTeam, sinnerName) {
   const checkbox = document.createElement('input');
@@ -15,7 +29,7 @@ function generateInTeamCheckbox(inTeam, sinnerName) {
   checkbox.checked = inTeam;
   checkbox.setAttribute('aria-label', `select ${sinnerName}`);
   checkbox.className =
-    'appearance-none peer w-8 h-8 bg-gray-300 border-gray-300 rounded focus:ring-red-500 focus:ring-2 checked:border-0dark:bg-gray-700 dark:border-gray-600 checked:bg-slate-300 dark:checked:bg-slate-500 dark:focus:ring-red-600 dark:ring-offset-gray-800';
+    'appearance-none peer w-8 h-8 bg-gray-300 border-gray-300 rounded focus:ring-red-500 focus:ring-2 checked:border-0 dark:bg-gray-700 dark:border-gray-600 checked:bg-slate-300 dark:checked:bg-slate-500 dark:focus:ring-red-600 dark:ring-offset-gray-800';
   checkbox.addEventListener('change', () => {
     team.toggle_sinner_selection(sinnerName);
     updateSins();
@@ -23,16 +37,18 @@ function generateInTeamCheckbox(inTeam, sinnerName) {
   return checkbox;
 }
 
-const sinnerContainers = document.getElementById('sinner-container').querySelectorAll('div.sinner-element');
-initialData.sinners.forEach((sinner, i) => {
-  const nameContainer = sinnerContainers[i].querySelector('p.name');
-  const selectedIdContainer = sinnerContainers[i].querySelector('p.selected-id');
-  const isInTeamCheckboxContainer = sinnerContainers[i].querySelector('span.in-team-checkbox-container');
-  nameContainer.innerHTML = sinner.name;
-  selectedIdContainer.innerHTML = sinner.selected_identity.name;
-  isInTeamCheckboxContainer.innerHTML = `<svg class="absolute w-8 h-8 right-0 top-1 fill-slate-900 hidden peer-checked:block pointer-events-none"><use xlink:href="#icon-id" /></svg>`;
-  isInTeamCheckboxContainer.prepend(generateInTeamCheckbox(sinner.in_team, sinner.name));
-});
+function initialRender() {
+  const sinnerContainers = document.getElementById('sinner-container').querySelectorAll('div.sinner-element');
+  initialData.sinners.forEach((sinner, i) => {
+    const nameContainer = sinnerContainers[i].querySelector('p.name');
+    const idContainer = sinnerContainers[i].querySelector('p.identity');
+    const isInTeamCheckboxContainer = sinnerContainers[i].querySelector('span.in-team-checkbox-container');
+    nameContainer.innerHTML = sinner.name;
+    isInTeamCheckboxContainer.prepend(generateInTeamCheckbox(sinner.in_team, sinner.name));
+    idContainer.appendChild(generateIdSelector(sinner.name, sinner.all_identities, sinner.selected_identity));
+  });
+  document.body.removeAttribute('style');
+}
 
 function updateSins() {
   const sinsContainer = document.getElementById('sins-container');
@@ -42,5 +58,5 @@ function updateSins() {
     sinsContainer.querySelector(`span.${sin}-sin-counter`).innerHTML = `${requiredSins[sin]}/${supportedSins[sin]}`;
   }
 }
-
+initialRender();
 updateSins();
