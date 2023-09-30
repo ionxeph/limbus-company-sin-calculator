6ed6536a2d9d12de7e88f8f2c172e8e426b3d3e0
andrew-cheng <andrew.cheng@gmail.com>
2023-09-29 21:48:32 -0500

    remove unnecessary configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const base = require('./webpack.base.config.js');
-const { merge } = require('webpack-merge');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = merge(base, {
-  mode: 'development',
+module.exports = {
+  entry: './bootstrap.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bootstrap.js',
+  },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      publicPath: '',
       template: './app/index.html',
       scriptLoading: 'defer',
     }),
+    new CopyPlugin({
+      patterns: [{ from: 'app/assets', to: 'assets' }],
+    }),
   ],
-});
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'app'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
+};
